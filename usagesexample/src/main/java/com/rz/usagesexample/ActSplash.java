81ce649c3f36f81fc4487a72a48ee4b80b7e14c8
Rz Rasel <Rz Rasel>
2017-12-12 00:22:01 +0600

    Add Projecr

--- usagesexample/src/main/java/com/rz/usagesexample/ActSplash.java
+++ usagesexample/src/main/java/com/rz/usagesexample/ActSplash.java
@@ -9,5 +9,12 @@ public class ActSplash extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.act_splash);
+        /*
+        Can the Android drawable folder contain subfolders
+        https://antonioleiva.com/android-multiple-resource-folders/
+        https://www.reddit.com/r/androiddev/comments/1pooce/how_to_organize_resdrawables/
+        http://babe.ilandroid.com/can-the-android-drawable-directory-contain-subdirectories.html
+        https://medium.com/google-developer-experts/android-project-structure-alternative-way-29ce766682f0
+        */
     }
 }
